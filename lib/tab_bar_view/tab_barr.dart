e6725304072abf5080329e56527f2a2a3b943372
Nas <nas.shihab92@gmail.com>
2023-03-29 16:42:54 +0600

    chng

--- lib/tab_bar_view/tab_barr.dart
+++ lib/tab_bar_view/tab_barr.dart
@@ -8,38 +8,44 @@ class TabBarr extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
+      length: 3,
       child: Scaffold(
-        appBar: myTabBar(context,
-            title: 'Tab Bar View',
-            leftIcon: const Icon(
-              Icons.arrow_back,
+        appBar: AppBar(
+          bottom: const TabBar(
+              labelColor: Colors.black,
+              labelStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              tabs: [
+                Tab(text: 'All'),
+                Tab(text: 'Mentioned'),
+                Tab(text: 'Detail'),
+              ]),
+          backgroundColor: Colors.transparent,
+          title: const FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Text(
+              'Tab Bar',
+              style: TextStyle(fontSize: 24, color: Colors.black),
+            ),
+          ),
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.keyboard_arrow_left,
               color: Colors.black,
             ),
-            rightIcon: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.search_sharp,
-                      size: 28,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const Icon(
-                    Icons.more_vert,
-                    size: 28,
-                    color: Colors.black,
-                  ),
-                ],
-              ),
-            )),
+          ),
+        ),
         body: TabBarView(
           children: [
-            allNotification(),
-            allNotification(),
+            myTabView(),
+            myTabView(),
+            myTabView(),
           ],
         ),
       ),
--- lib/tab_bar_view/tab_barr_widget.dart
+++ lib/tab_bar_view/tab_barr_widget.dart
@@ -1,41 +1,7 @@
 import 'package:flutter/material.dart';
 
-AppBar myTabBar(
-  BuildContext context, {
-  required String title,
-  Widget rightIcon = const SizedBox.shrink(),
-  Widget leftIcon = const SizedBox.shrink(),
-}) =>
-    AppBar(
-      bottom: const TabBar(
-          labelColor: Colors.black,
-          labelStyle: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-          tabs: [
-            Tab(text: 'All'),
-            Tab(text: 'Mentioned'),
-          ]),
-      backgroundColor: Colors.transparent,
-      title: FittedBox(
-        fit: BoxFit.fitWidth,
-        child: Text(
-          title,
-          style: const TextStyle(fontSize: 24, color: Colors.black),
-        ),
-      ),
-      actions: [rightIcon],
-      elevation: 0,
-      leading: IconButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        icon: leftIcon,
-      ),
-    );
 
-Widget allNotification() => ListView.builder(
+Widget myTabView() => ListView.builder(
       itemCount: 5,
       itemBuilder: (context, index) => Padding(
         padding: const EdgeInsets.symmetric(vertical: 10),
